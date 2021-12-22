8cf734dcb2d2e09df741880e4a27f6c5d8437a91
Saltedasparagus <Coty.gunnett@gmail.com>
2021-12-21 23:17:20 -0600

    Added authentication for login
    Added the authentication for login/logout/register
    
    Ready for review and pull

---
+++ src/main/java/com/tappyplanner/models/data/UserRepository.java
@@ -0,0 +1,12 @@
+package com.tappyplanner.models.data;
+
+import com.tappyplanner.models.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+    User findByUsername(String username);
+
+}
