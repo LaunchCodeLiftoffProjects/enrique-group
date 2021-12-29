bf20b0c9cc9b4a14da5b60b1f57cc78ec66c9011
djion1820 <84429546+djion1820@users.noreply.github.com>
2021-12-28 23:20:21 -0600

    I finished the homepage and added a navigation tab for the login and register.

--- src/main/java/com/tappyplanner/controllers/HomeController.java
+++ src/main/java/com/tappyplanner/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package com.tappyplanner.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /* Giovanni Brex created
@@ -12,8 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    @RequestMapping("")
-    public String index() {
+    @RequestMapping("index")
+    public String index(Model model) {
+        model.addAttribute("title", "My Tabby List");
+        model.addAttribute("jobs");
 
         return "index";
     }
