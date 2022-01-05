617c784c0e4e9857fd7a739ee9c658d9df80d112
Giovanni Brex <Giovanni.Brex@hotmail.com>
2022-01-05 09:23:37 -0600

    Added Task class with description, date, and reminder fields.

---
+++ src/main/java/com/tappyplanner/models/Task.java
@@ -0,0 +1,64 @@
+package com.tappyplanner.models;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+/* Giovanni Brex created
+ * on 1/4/2022 inside
+ * the package -
+ * com.tappyplanner.models */
+
+
+@Entity
+public class Task extends AbstractEntity{
+
+    @Size(min = 3, max = 250 , message = "Description must be between 3 and 250 characters.")
+    private String description;
+
+    @NotNull(message = "Date is required.")
+    private Date date;
+
+    //TODO implement functionality that connects with user email or phone api
+    //     to send a reminder at a predetermined time
+
+
+    @NotNull(message = "Please choose if you'd like to be reminded of this task.")
+    private Boolean reminder;
+
+    public Task() {
+
+    }
+
+    public Task(String description, Date date, Boolean reminder) {
+        super();
+        this.description = description;
+        this.date = date;
+        this.reminder = reminder;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Boolean getReminder() {
+        return reminder;
+    }
+
+    public void setReminder(Boolean reminder) {
+        this.reminder = reminder;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
